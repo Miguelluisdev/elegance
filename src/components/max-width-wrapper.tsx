345132685b4dd68b79ad332a64c6507e90af511c
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-07-05 01:22:18 -0300

    styles: add animations with framer motion

--- src/components/max-width-wrapper.tsx
+++ src/components/max-width-wrapper.tsx
@@ -1,21 +1,31 @@
-import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
+"use client"
+import { cn } from "@/lib/utils"
+import { motion } from "framer-motion"
+import { ReactNode } from "react"
+import { variant1 } from "./animation"
 
 export const MaxWidthWrapper = ({
   className,
   children,
 }: {
-  className?: string;
-  children: ReactNode;
+  className?: string
+  children: ReactNode
 }) => {
   return (
-    <div
-      className={cn(
-        "mx-auto w-full h-full  max-w-screen-2xl px-2.5",
-        className
-      )}
+    <motion.div
+      variants={variant1(0.5)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
     >
-      {children}
-    </div>
-  );
-};
+      <div
+        className={cn(
+          "mx-auto w-full h-full  max-w-screen-2xl px-2.5",
+          className,
+        )}
+      >
+        {children}
+      </div>
+    </motion.div>
+  )
+}
